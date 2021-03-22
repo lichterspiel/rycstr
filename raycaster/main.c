608b881eec467fbdead8e307660fe2e6ba3ecd8e
lichterspiel <mehmetxeris@gmail.com>
2021-03-22 19:52:14 +0100

    Initial Commit

---
+++ raycaster/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  raycaster
+//
+//  Created by Mehmet Sinan Eris on 22.03.21.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
